7b816ea732984565e03801d773da516be905c2b3
Marcela Rodrigues Garcia <mrodriguesg16@gmail.com>
2022-02-01 08:42:07 -0300

    Interview Prep. Kit
    https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps

---
+++ HackerRank_2Strings.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'twoStrings' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+def twoStrings(s1, s2):
+    set1 = set(s1)
+    set2 = set(s2)
+
+    return 'NO' if set1.isdisjoint(set2) else 'YES'
+
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = sys.stdout
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s1 = input()
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
