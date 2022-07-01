8bac5adefccec78f8fb069bc1b174f3edec57482
Marcela Rodrigues Garcia <mrodriguesg16@gmail.com>
2022-07-01 08:55:30 -0300

    + coding

---
+++ HackerRank_CompareTriplets.py
@@ -0,0 +1,45 @@
+# https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=false&h_r=next-challenge&h_v=zen
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    alice = 0
+    bob = 0
+
+    for i in range(len(a)):
+        if a[i] > b[i]:
+            alice = alice + 1
+        elif b[i] > a[i]:
+            bob = bob + 1
+
+    return [alice, bob]
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = sys.stdout
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ HackerRank_SolveMe1st.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=false
+
+def solveMeFirst(a,b):
+	return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
