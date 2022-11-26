7b706f907b93a6603561205b7142b8b68d3456d2
Marcela Rodrigues Garcia <mrodriguesg16@gmail.com>
2022-11-26 08:44:38 -0300

    + coding

--- LeetCode_Trees.py
+++ LeetCode_Trees.py
@@ -5,15 +5,50 @@ class TreeNode(object):
         self.left = left
         self.right = right
 
-def preorderTraversal(root, order):
-    if root:
-        order.append(root.val)
-        preorderTraversal(root.left,order)
-        preorderTraversal(root.right,order)
-    return order
+class Solution(object):
+    #Pre Order
+    #https://leetcode.com/problems/binary-tree-preorder-traversal
+    def preorderTraversal(self, root):
+        res = []
+        self.dfsPre(root, res)
+        return res
+
+    def dfsPre(self, root, res):
+        if root:
+            res.append(root.val)
+            self.dfsPre(root.left, res)
+            self.dfsPre(root.right, res)
+
+    #In Order
+    #https://leetcode.com/problems/binary-tree-inorder-traversal
+    def inorderTraversal(self, root):
+        res = []
+        self.dfsIn(root, res)
+        return res
+
+    def dfsIn(self, root, res):
+        if root:
+            self.dfsIn(root.left, res)
+            res.append(root.val)
+            self.dfsIn(root.right, res)
+
+    #Post Order
+    #https://leetcode.com/problems/binary-tree-postorder-traversal
+    def postorderTraversal(self, root):
+        res = []
+        self.dfsPost(root, res)
+        return res
+
+    def dfsPost(self, root, res):
+        if root:
+            self.dfsPost(root.left, res)
+            self.dfsPost(root.right, res)
+            res.append(root.val)
+
 
 tree = TreeNode(1)
 tree.right = TreeNode(2)
 tree.right.left = TreeNode(3)
 
-print(preorderTraversal(tree,[]))
+sol = Solution()
+print(sol.preorderTraversal(tree,[]))
